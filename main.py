a8616a3cf7ecf53ea3fd5783ac173b9424609d08
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-09-02 17:54:24 +0300

    final

--- main.py
+++ main.py
@@ -5,6 +5,47 @@ import argparse
 import random
 import copy
 import os
+import time
+
+def neigboor_count(maps, strin, el, animal, height, width):
+    fish_neighb = 0
+    if el > 0 and maps[strin][el - 1] == animal:
+        fish_neighb += 1
+    if el + 1 < width and maps[strin][el + 1] == animal:
+        fish_neighb += 1
+    if strin + 1 < height and maps[strin + 1][el] == animal:
+        fish_neighb += 1
+    if strin > 0 and maps[strin - 1][el] == animal:
+        fish_neighb += 1
+    if strin > 0 and el > 0 and maps[strin - 1][el - 1] == animal:
+        fish_neighb += 1
+    if strin > 0 and el + 1 < width and maps[strin - 1][el + 1] == animal:
+        fish_neighb += 1
+    if el + 1 < width and strin + 1 < height and maps[strin + 1][el + 1] == animal:
+        fish_neighb += 1
+    if el > 0 and strin + 1 < height and maps[strin + 1][el - 1] == animal:
+        fish_neighb += 1
+    return fish_neighb
+
+def change_el(maps, strin, el, height, width):
+    if maps[strin][el] == '🗿':
+        return '🗿'
+    if maps[strin][el] == '🐠' or maps[strin][el] == '🦐':
+        animal = maps[strin][el]
+        fish_neighb = neigboor_count(maps, strin, el, animal, height, width)
+        if fish_neighb < 2 or fish_neighb > 3:
+            return '🌊'
+        else:
+            return animal
+    else:
+        fish_neighb = neigboor_count(maps, strin, el, '🐠', height, width)
+        shrimp_neighb = neigboor_count(maps, strin, el, '🦐', height, width)
+        if fish_neighb == 3:
+            return '🐠'
+        if shrimp_neighb == 3:
+            return '🦐'
+        return '🌊'
+
 
 
 rand = ['🦐', '🐠', '🗿', '🌊']
@@ -21,9 +62,10 @@ for strin in range(height):
     for el in range(width):
         maps[strin][el] = random.choice(rand)
 while True:
-    for strin in maps:
-        for el in strin:
-            print(el, end='', sep='')
-        print()
-    break
     os.system('cls' if os.name == 'nt' else 'clear')
+    for strin in range(height):
+        for el in range(width):
+            print(maps[strin][el], end='', sep='')
+            maps[strin][el] = change_el(maps, strin, el, height, width)
+        print()
+    time.sleep(2)
